d5df94846375d219b8f725f369c04635f5449070
adney11 <adney11@gmail.com>
2023-01-08 15:25:31 -0700

    run_all_traces prepped for below6mbps_test_mahimahi

--- customizable_http_video_server/run_all_traces.sh
+++ customizable_http_video_server/run_all_traces.sh
@@ -1,24 +1,33 @@
 #!/bin/bash -x
 
 abrs=("RL")
-traces=$1
-scheme=$2
-all_traces=$3
-donelog="./"
+traces='/newhome/Orca/below6mbps_test_mahimahi/' #$1
+scheme=$1
+all_traces='/newhome/Orca/traces/' #$3
+donedir='/newhome/Orca/below6mbps_test_mahimahi_done/'
+limit=10
 
 
 for abr in ${abrs[@]}
 do
+    count=0
     for trace in $traces*
     do
         tracename=$(basename $trace)
         echo "./run_trace.sh 44444 $abr $all_traces $tracename $scheme ."
         ./run_trace.sh 44444 $abr $all_traces $tracename $scheme .
-        mv $trace /newhome/Orca/fcc_traces_done/
+        mv $trace $donedir
+        count=$((count + 1))
+        if [[ "$count" -eq $limit ]];
+        then
+            echo "finished $limit traces"
+            break
+        fi
         sleep 5
     done
 
 done
+echo "finished run_all_traces.sh"
 
 # Run using following command
 # ./run_all_traces.sh /newhome/Orca/fcc_traces/ bbr /newhome/Orca/traces/
